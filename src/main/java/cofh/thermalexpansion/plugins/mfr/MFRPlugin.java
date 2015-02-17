c476ce1a19cd35cf5f12f7254fafdcc4dd06979f
skyboy <skyboy026@gmail.com>
2015-02-17 06:28:38 -0500

    Add MFR support for fertilizer

--- src/main/java/cofh/thermalexpansion/plugins/mfr/MFRPlugin.java
+++ src/main/java/cofh/thermalexpansion/plugins/mfr/MFRPlugin.java
@@ -2,9 +2,15 @@ package cofh.thermalexpansion.plugins.mfr;
 
 import cofh.lib.util.helpers.MathHelper;
 import cofh.thermalexpansion.ThermalExpansion;
+import cofh.thermalexpansion.item.TEItems;
 import cpw.mods.fml.common.Loader;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
 import powercrystals.minefactoryreloaded.api.FactoryRegistry;
+import powercrystals.minefactoryreloaded.api.FertilizerType;
+import powercrystals.minefactoryreloaded.api.IFactoryFertilizer;
 import powercrystals.minefactoryreloaded.api.ValuedItem;
 
 public class MFRPlugin {
@@ -51,6 +57,36 @@ public class MFRPlugin {
 			if (strawCoal) {
 				FactoryRegistry.sendMessage("registerLiquidDrinkHandler", new ValuedItem("coal", DrinkHandlerCoal.instance));
 			}
+			FactoryRegistry.sendMessage("registerFertilizer", new IFactoryFertilizer() {
+
+				@Override
+				public Item getFertilizer() {
+
+					return TEItems.itemMaterial;
+				}
+
+				@Override
+				public FertilizerType getFertilizerType(ItemStack stack) {
+
+					if (TEItems.fertilizer.isItemEqual(stack))
+						return FertilizerType.GrowPlant;
+					else if (TEItems.fertilizerRich.isItemEqual(stack))
+						return FertilizerType.GrowPlant;
+					return FertilizerType.None;
+				}
+
+				@Override
+				public void consume(ItemStack fertilizer) {
+
+					if (TEItems.fertilizerRich.isItemEqual(fertilizer)) {
+						if (MathHelper.RANDOM.nextBoolean())
+							fertilizer.stackSize += 1;
+					} else
+						fertilizer.stackSize -= 1;
+					fertilizer.stackSize -= 1;
+				}
+
+			});
 			ThermalExpansion.log.info("MineFactoryReloaded Plugin Enabled.");
 		}
 	}
