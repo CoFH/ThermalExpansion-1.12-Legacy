319603eabf817ffb9c6e1ed81c8215300acfc290
King Lemming <kinglemming@gmail.com>
2015-02-25 22:03:37 -0500

    Removed debugish...code.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalexpansion/block/light/BlockLight.java
+++ src/main/java/cofh/thermalexpansion/block/light/BlockLight.java
@@ -73,7 +73,6 @@ public class BlockLight extends BlockTEBase implements IBlockConfigGui {
 			TileLight tile = (TileLight) world.getTileEntity(x, y, z);
 
 			if (stack.stackTagCompound.hasKey("Color")) {
-				System.out.println("yup fucked");
 				tile.setColor(stack.stackTagCompound.getInteger("Color"));
 			}
 			tile.dim = stack.stackTagCompound.getBoolean("Dim");
@@ -211,11 +210,11 @@ public class BlockLight extends BlockTEBase implements IBlockConfigGui {
 		}
 		if (enable[Types.LAMP_HALO.ordinal()]) {
 			GameRegistry.addRecipe(new ShapedOreRecipe(ItemHelper.cloneStack(lampBasic, 4), new Object[] { " L ", "GLG", " S ", 'L', "ingotLumium", 'G',
-					"blockGlassHardened", 'S', "ingotSignalum" }));
+				"blockGlassHardened", 'S', "ingotSignalum" }));
 		}
 		if (enable[Types.LAMP_BASIC.ordinal()]) {
 			GameRegistry.addRecipe(new ShapedOreRecipe(ItemHelper.cloneStack(lampBasicAlt, 4), new Object[] { " L ", "GLG", " S ", 'L', "dustLumium", 'G',
-					"blockGlassHardened", 'S', "ingotSignalum" }));
+				"blockGlassHardened", 'S', "ingotSignalum" }));
 		}
 		return true;
 	}
