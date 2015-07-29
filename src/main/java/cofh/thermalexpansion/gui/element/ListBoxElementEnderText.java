78791524ccabfb0c13f269ce0c42b5cd5a981dcd
skyboy <mr.wraith.06@gmail.com>
2015-07-29 19:10:58 -0400

    Fix Ender ListBox Text element

--- src/main/java/cofh/thermalexpansion/gui/element/ListBoxElementEnderText.java
+++ src/main/java/cofh/thermalexpansion/gui/element/ListBoxElementEnderText.java
@@ -2,6 +2,7 @@ package cofh.thermalexpansion.gui.element;
 
 import cofh.lib.gui.element.ElementListBox;
 import cofh.lib.gui.element.listbox.ListBoxElementText;
+import cofh.lib.transport.IEnderChannelRegistry.Frequency;
 import cofh.thermalfoundation.fluid.TFFluids;
 
 import net.minecraftforge.fluids.FluidStack;
@@ -9,19 +10,25 @@ import net.minecraftforge.fluids.FluidStack;
 
 public class ListBoxElementEnderText extends ListBoxElementText {
 
-	public final int freq;
+	private final Frequency freq;
 
-	public ListBoxElementEnderText(String text, int freq) {
+	public ListBoxElementEnderText(Frequency freq) {
 
-		super(text);
+		super(freq.name);
 		this.freq = freq;
+	}
+
+	@Override
+	public Object getValue() {
+
+		return freq;
 	}
 
 	@Override
 	public void draw(ElementListBox listBox, int x, int y, int backColor, int textColor) {
 
 		if (backColor == 1) {
-			listBox.getContainerScreen().drawFluid(x, y, new FluidStack(TFFluids.fluidEnder, 1000), getWidth(), getHeight());
+			listBox.getContainerScreen().drawFluid(x, y, new FluidStack(TFFluids.fluidEnder, 1000), Math.max(getWidth(), listBox.getContentWidth()), getHeight());
 		}
 		super.draw(listBox, x, y, backColor, textColor);
 	}
