1efb33bac8f79b6a04f31fbff1f074ee9055a73e
skyboy <mr.wraith.06@gmail.com>
2015-08-11 19:52:38 -0400

    Remove debug particles

--- src/main/java/cofh/thermalexpansion/block/simple/BlockAirForce.java
+++ src/main/java/cofh/thermalexpansion/block/simple/BlockAirForce.java
@@ -1,13 +1,6 @@
 package cofh.thermalexpansion.block.simple;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
-import java.util.Random;
-
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.EntityFX;
-import net.minecraft.client.particle.EntityFireworkSparkFX;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -52,27 +45,8 @@ public class BlockAirForce extends BlockAirBase {
 			ent.motionY = 0;
 		}
 		/*
-		 * Meta maps to ForgeDirection:
-		 * ^ move entity to last position, then move pos and last pos by a specified amount (anti-gravity)
-		 * ^ can we dampen sound effects for an entity collided with this?
+		 * can we dampen sound effects for an entity collided with this?
 		 */
-	}
-
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
-
-		EntityFireworkSparkFX spark = new EntityFireworkSparkFX(world, x + 0.5 + rand.nextGaussian() * 0.1, y + 0.5 + rand.nextGaussian() * 0.1, z + 0.5
-				+ rand.nextGaussian() * 0.1, 0, 0, 0, Minecraft.getMinecraft().effectRenderer) {
-
-			@Override
-			public void moveEntity(double x, double y, double z) {
-
-			}
-		};
-		spark.setColour(0xFF66FF);
-		spark.setFadeColour(0xFFAAFF);
-		Minecraft.getMinecraft().effectRenderer.addEffect(spark);
 	}
 
 }
