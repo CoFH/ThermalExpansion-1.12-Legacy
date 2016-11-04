edbdef9b676c65161e786933ac55af1434ed143a
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-11-04 17:41:12 +0100

    Made bounding box static and added cache key for sponges

--- src/main/java/cofh/thermalexpansion/block/simple/BlockAirBase.java
+++ src/main/java/cofh/thermalexpansion/block/simple/BlockAirBase.java
@@ -18,6 +18,7 @@ import java.util.Random;
 
 public class BlockAirBase extends Block {
 
+    private static final AxisAlignedBB EMPTY_BB = new AxisAlignedBB(0D, 0D, 0D, 0D, 0D, 0D);
     public static final Material materialBarrier = new Material(MapColor.AIR).setReplaceable();
 
     public BlockAirBase(Material material) {
@@ -34,7 +35,7 @@ public class BlockAirBase extends Block {
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        return new AxisAlignedBB(0D, 0D, 0D, 0D, 0D, 0D);
+        return EMPTY_BB;
     }
 
     @Override
