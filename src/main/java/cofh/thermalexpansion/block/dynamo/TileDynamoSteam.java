94df3b1c86aedfae757c39669cdb8816c99ecd68
King Lemming <kinglemming@gmail.com>
2017-03-05 14:14:34 -0500

    Fix Steam Dynamo. Update Chinese localization.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalexpansion/block/dynamo/TileDynamoSteam.java
+++ src/main/java/cofh/thermalexpansion/block/dynamo/TileDynamoSteam.java
@@ -18,10 +18,8 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTankInfo;
@@ -359,7 +357,7 @@ public class TileDynamoSteam extends TileDynamoBase {
 		}
 		int energy = fuels.get(new ComparableItemStack(stack));
 
-		return energy > 0 ? energy : TileEntityFurnace.getItemBurnTime(stack) * CoreProps.RF_PER_MJ * 3 / 2;
+		return energy > 0 ? energy : GameRegistry.getFuelValue(stack) * CoreProps.RF_PER_MJ * 3 / 2;
 	}
 
 }
