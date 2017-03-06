8fe7a8de60e8a2ddd69697cab3887b862172a2ce
King Lemming <kinglemming@gmail.com>
2017-03-05 23:56:34 -0500

    Update to 5.0.1.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalexpansion/ThermalExpansion.java
+++ src/main/java/cofh/thermalexpansion/ThermalExpansion.java
@@ -37,7 +37,7 @@ public class ThermalExpansion {
 	public static final String MOD_ID = "thermalexpansion";
 	public static final String MOD_NAME = "Thermal Expansion";
 
-	public static final String VERSION = "5.0.0";
+	public static final String VERSION = "5.0.1";
 	public static final String VERSION_MAX = "5.1.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
