7ae2b873cd397be270ec70f1b8e504589b697efd
King Lemming <kinglemming@gmail.com>
2017-06-15 15:13:33 -0400

    Rectify rebase conflicts.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalexpansion/plugins/jei/crafting/sawmill/SawmillRecipeWrapper.java
+++ src/main/java/cofh/thermalexpansion/plugins/jei/crafting/sawmill/SawmillRecipeWrapper.java
@@ -53,7 +53,7 @@ public class SawmillRecipeWrapper extends BaseRecipeWrapper {
 
 		if (ComparableItemStackSawmill.getOreID(recipe.getInput()) != -1) {
 			for (ItemStack ore : OreDictionary.getOres(ItemHelper.getOreName(recipe.getInput()), false)) {
-				recipeInputs.add(ItemHelper.cloneStack(ore, recipe.getInput().stackSize));
+				recipeInputs.add(ItemHelper.cloneStack(ore, recipe.getInput().getCount()));
 			}
 		} else {
 			recipeInputs.add(recipe.getInput());
