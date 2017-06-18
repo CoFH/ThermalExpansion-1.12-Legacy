b68aea7c9e55afbff71b8f65aca5097cb1225771
King Lemming <kinglemming@gmail.com>
2017-06-18 08:40:11 -0400

    Stupid-proof the tank to prevent mods doing irresponsible blanket scans from breaking things.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermalexpansion/ThermalExpansion.java
+++ src/main/java/cofh/thermalexpansion/ThermalExpansion.java
@@ -38,7 +38,7 @@ public class ThermalExpansion {
 	public static final String MOD_ID = "thermalexpansion";
 	public static final String MOD_NAME = "Thermal Expansion";
 
-	public static final String VERSION = "5.2.1";
+	public static final String VERSION = "5.2.2";
 	public static final String VERSION_MAX = "5.3.0";
 	public static final String VERSION_GROUP = "required-after:" + MOD_ID + "@[" + VERSION + "," + VERSION_MAX + ");";
 	public static final String UPDATE_URL = "https://raw.github.com/cofh/version/master/" + MOD_ID + "_update.json";
